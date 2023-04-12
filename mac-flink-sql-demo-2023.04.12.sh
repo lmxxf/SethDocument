46ad4acfe4ed64fd4f805b7b0b45df4e383dcd13
jinyanyan <jinyanyan@tantanapp.com>
2023-04-12 11:28:40 +0800

    体验flink sql

---
+++ mac-flink-sql-demo-2023.04.12.sh
@@ -0,0 +1,44 @@
+# 启动flink
+cd /usr/local/Cellar/apache-flink/1.16.1
+./libexec/bin/start-cluster.sh
+
+
+# 启动flink-sql-client
+./libexec/bin/sql-client.sh embedded
+
+
+# 在提示符中输入
+<<EOF
+CREATE TEMPORARY TABLE input (
+  id INT,
+  name STRING,
+  age INT
+) WITH (
+  'connector' = 'filesystem',
+  'path' = 'file:///Users/yanyanjin/Study/FlinkStudy/FlinkQuickstartDemo1/src/main/resources/input.csv',
+  'format' = 'csv'
+);
+EOF
+
+
+# 输入SELECT后，表格信息将展示在一个类似于只读的文字编辑器中
+<<EOF
+SELECT id, name, age + 1 as age_plus_one FROM input;
+EOF
+
+
+# csv文件
+# 路径为: /Users/yanyanjin/Study/FlinkStudy/FlinkQuickstartDemo1/src/main/resources/input.csv
+# 内容为:
+<<EOF
+1,Alice,30
+2,Bob,28
+3,Charlie,22
+EOF
+
+# 最后退出sql-client
+<<EOF
+EXIT;
+# 或者
+QUIT;
+EOF
